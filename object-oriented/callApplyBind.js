aeb9e7020f05657de644bc511bc6451362d688c7
Samrat798 <majumdersamrat26@gmail.com>
2024-04-10 18:44:45 +0530

    aster

--- object-oriented/callApplyBind.js
+++ object-oriented/callApplyBind.js
@@ -1,5 +1,7 @@
 //  call -runs instantly, arguments - list of items
+// The below code for call
 
+/*
 const john = {
   name: 'John',
   age: 24,
@@ -29,3 +31,58 @@ greet.call({ name: 'Peter', age: 30 });
 
 // another way to call another objects methods
 john.greet.call(sam);
+*/
+
+// apply - runs instantly, arguments - array of items
+//  the below code for apply
+
+/*
+const john = {
+  name: 'john',
+  age: 24,
+};
+
+const susan = {
+  name: 'susan',
+  age: 27,
+};
+
+function greet(city, country) {
+  console.log(this);
+  console.log(
+    `Hello, I'm ${this.name} and I'm ${this.age} years old and i live in ${city}, ${country}`
+  );
+}
+
+// greet.call(john, 'san diego', 'us');
+// greet.call(susan, 'san diego', 'us');
+// greet.call({ name: 'peter', age: 30 }, 'san diego', 'us');
+
+greet.apply(john, ['san diego', 'us']);
+greet.apply(susan, ['san diego', 'us']);
+greet.apply({ name: 'peter', age: 30 }, ['san diego', 'us']);
+*/
+
+// Bind - assign, use later, arguments - list of items
+//  Check below code for bind
+
+const john = {
+  name: 'john',
+  age: 24,
+};
+
+const susan = {
+  name: 'susan',
+  age: 27,
+};
+
+function greet(city, country) {
+  console.log(this);
+  console.log(
+    `Hello, I'm ${this.name} and I'm ${this.age} years old and i live in ${city}, ${country}`
+  );
+}
+
+// assign, call it later
+const susanGreet = greet.bind(susan, 'toronto', 'canada');
+susanGreet();
