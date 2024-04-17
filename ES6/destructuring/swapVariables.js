a7c41dfd37cfd3501cf57ad5d6657bda8aeb8923
Samrat798 <majumdersamrat26@gmail.com>
2024-04-17 22:37:17 +0530

    aster

---
+++ ES6/destructuring/swapVariables.js
@@ -0,0 +1,16 @@
+// Destructuring swap variables
+// faster/easier way to access/unpack values form array
+// objects into variables
+// Arrays
+
+let first = 'bob';
+let second = 'John';
+
+// let temp = first;
+// first = second;
+// second = temp;
+
+[second, first] = [first, second];
+
+console.log(first, second);
+3;
