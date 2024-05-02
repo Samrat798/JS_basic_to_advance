3d2f7ea346880fbfc24c9e42954671927f43164f
Samrat798 <majumdersamrat26@gmail.com>
2024-05-02 13:05:48 +0530

    aster

---
+++ modules/utils/data.js
@@ -0,0 +1,7 @@
+export const random = 'random value';
+export const people = [
+  { name: 'John', job: 'developer' },
+  { name: 'Susan', job: 'designer' },
+  { name: 'Anna', job: 'the boss' },
+  { name: 'peter', job: 'developer' },
+];
