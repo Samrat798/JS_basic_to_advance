eeb58e1e87793efc7b5ef1180a001d49e701c26c
Samrat798 <majumdersamrat26@gmail.com>
2024-05-06 00:41:45 +0530

    aster

---
+++ asyncJS/app.js
@@ -0,0 +1,16 @@
+// Javascript is single threaded, synchronous language.
+
+console.log(`I'm first`);
+console.log(`I'm second`);
+console.log(`I'm thored`);
+
+console.log(`I'm first`);
+boilingWater();
+console.log(`I'm thored`);
+function boilingWater() {
+  console.log('boiling.....');
+  for (let i = 0; i < 100000; i++) {
+    console.log('Still not done...');
+  }
+  console.log('Done..');
+}
