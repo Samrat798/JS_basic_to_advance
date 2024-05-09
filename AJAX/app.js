07da306e1fe745e6315734cca3bf31190c967ac1
Samrat798 <majumdersamrat26@gmail.com>
2024-05-09 11:52:52 +0530

    aster

---
+++ AJAX/app.js
@@ -0,0 +1,16 @@
+const xhr = new XMLHttpRequest();
+
+console.log(xhr);
+
+xhr.open('GET', './api/sample.txt');
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log('done');
+  } else {
+    console.log({
+      status: xhr.status,
+      text: xhr.statusText,
+    });
+  }
+};
+xhr.send();
